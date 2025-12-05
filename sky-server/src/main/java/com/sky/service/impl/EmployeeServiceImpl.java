ef0c3fd10c3bdbb7cc4f577bb03e33d55550fdd2
ts5211314 <3396617943@qq.com>
2025-12-05 17:20:59 +0800

    启用禁用员工

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -102,4 +102,15 @@ public class EmployeeServiceImpl implements EmployeeService {
         return new PageResult(total , result);
     }
 
+    /**
+     * 启用禁用员工
+     * @param status
+     * @param id
+     */
+    @Override
+    public void startOrStop(Integer status, Long id) {
+        Employee employee = Employee.builder().status(status).id(id).build();
+        employeeMapper.update(employee);
+    }
+
 }
