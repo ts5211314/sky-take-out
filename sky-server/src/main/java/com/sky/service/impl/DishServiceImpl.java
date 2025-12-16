e9ce3efd591b459a4dfb90d0d41e57553f1fa7cc
ts5211314 <3396617943@qq.com>
2025-12-16 16:22:13 +0800

    套餐管理

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -129,6 +129,11 @@ public class DishServiceImpl implements DishService {
         }
     }
 
+    /**
+     * 菜品启用停用
+     * @param status
+     * @param id
+     */
     @Override
     public void startOrStop(Integer status, Long id) {
         Dish dish = Dish.builder()
@@ -138,7 +143,18 @@ public class DishServiceImpl implements DishService {
         dishMapper.update(dish);
     }
 
-
+    /**
+     * 根据分类id查询菜品
+     * @param categoryId
+     * @return
+     */
+    public List<Dish> list(Long categoryId) {
+        Dish dish = Dish.builder()
+                .categoryId(categoryId)
+                .status(StatusConstant.ENABLE)
+                .build();
+        return dishMapper.list(dish);
+    }
 
 
 }
